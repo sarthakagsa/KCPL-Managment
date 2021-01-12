403e702a128f8a959d5dca21f6262bfafda26439
sarthakagsa <sarthakag.sa@gmail.com>
2021-01-12 12:22:24 +0530

    Repair model fixed, no frontend included(only API)

--- routes/repair.js
+++ routes/repair.js
@@ -28,14 +28,14 @@ router.post('/repair/:id',auth,async(req,res)=>{
 
 router.patch('/repair/:vechileid/:repairid',auth,async(req,res)=>{
     const updates = Object.keys(req.body)
-    const allowedUpdates =['parts','date','cost']
+    const allowedUpdates =['parts','date','cost','invoice','partyname','km','gst']
     const isValidOperation = updates.every((update)=> allowedUpdates.includes(update))
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid Updates!'})       
     }
     req.body.parts.forEach(element => {
         const partsUpdates = Object.keys(element)
-        const partsAllowedUpdates = ['partname','cost']
+        const partsAllowedUpdates = ['partname','cost','quantity','hsn']
         const isValidOperation = partsUpdates.every((update)=>partsAllowedUpdates.includes(update))
         if (!isValidOperation) {
             return res.status(400).send({ error: 'Invalid Updates!'})       
@@ -51,7 +51,7 @@ router.patch('/repair/:vechileid/:repairid',auth,async(req,res)=>{
 
         const repair = await Repair.findOne({_id: req.params.repairid,vechileid:req.params.vechileid})
         if (!repair) {
-            return res.status(404).send({error : "Tthe following docs is not found"})
+            return res.status(404).send({error : "The following docs is not found"})
         }
         updates.every((update)=> repair[update] = req.body[update] )
         await repair.save()
